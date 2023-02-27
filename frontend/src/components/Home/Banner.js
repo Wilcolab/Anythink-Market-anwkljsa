a7de010fe1e743ac9fcc3042b2bcf4660082f570
OB <85952901+177boris@users.noreply.github.com>
2023-02-27 23:55:59 +0000

    create search bar on the frontend ui

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -9,6 +9,12 @@ const Banner = () => {
         <div>
           <span>A place to </span>
           <span id="get-part">get</span>
+
+            <input 
+            id="search-box" 
+            type="text" 
+            placeholder="What do you desire?"/>
+            
           <span> the cool stuff.</span>
         </div>
       </div>
